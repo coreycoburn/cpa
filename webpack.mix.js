a6b4d34c12611729c7ac9f408167e7df5be0aaee
CPA Valet <cpavalet@Coreys-Mac-mini.local>
2018-12-05 09:43:06 -0800

    Upgraded to Laravel 5.7 and installed Laravel Telescope

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,9 +2,9 @@ let mix = require("laravel-mix");
 let tailwindcss = require("tailwindcss");
 require("laravel-mix-purgecss");
 
-mix.js("resources/assets/js/app.js", "public/js")
-    .copyDirectory("resources/assets/img", "public/img")
-    .sass("resources/assets/scss/app.scss", "public/css")
+mix.js("resources/js/app.js", "public/js")
+    .copyDirectory("resources/img", "public/img")
+    .sass("resources/scss/app.scss", "public/css")
     .options({
         processCssUrls: false,
         postCss: [tailwindcss("./tailwind.js")]
